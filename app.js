c4fc69f17ddfc72657c8b1558ae7c2cf3aeac244
데스크탑 <lleellee013@gmail.com>
2017-10-09 23:50:59 +0900

    불필요한 주석 제거, 가격 클릭 시 해당 거래소 해당 코인 거래 창으로 이동하는 링크 추가(새창으로 열기)

--- app.js
+++ app.js
@@ -26,23 +26,23 @@ app.listen(3000, () => {
 
 
 const makeTotalTable = () => {
-    for(let i = 0; i < poloniex.poloniexLastPriceArray.length; i++) {
+    for (let i = 0; i < poloniex.poloniexLastPriceArray.length; i++) {
         let obj = {};
         obj.name = poloniex.poloniexLastPriceArray[i].name.replace('_', '-');
         totalTable.push(obj);
     }
 
-    for(let i = 0; i < bittrex.bittrexLastPriceArray.length; i++) {
+    for (let i = 0; i < bittrex.bittrexLastPriceArray.length; i++) {
         let flag = false;
-        for(let j = 0; j < totalTable.length; j++) {
-            if(bittrex.bittrexLastPriceArray[i].name === totalTable[j].name) {
+        for (let j = 0; j < totalTable.length; j++) {
+            if (bittrex.bittrexLastPriceArray[i].name === totalTable[j].name) {
                 flag = false;
                 break;
             } else {
                 flag = true;
             }
         }
-        if(flag === true) {
+        if (flag === true) {
             let obj = {};
             obj.name = bittrex.bittrexLastPriceArray[i].name;
             totalTable.push(obj);
@@ -53,32 +53,32 @@ const makeTotalTable = () => {
 
 const refreshTotalTable = () => {
     console.log(`refreshTotalTable is called!`);
-    for(let i = 0; i < poloniex.poloniexLastPriceArray.length; i++) {
-        for(let j = 0; j < totalTable.length; j++) {
-            if(poloniex.poloniexLastPriceArray[i].name.replace('_', '-') === totalTable[j].name) {
+    for (let i = 0; i < poloniex.poloniexLastPriceArray.length; i++) {
+        for (let j = 0; j < totalTable.length; j++) {
+            if (poloniex.poloniexLastPriceArray[i].name.replace('_', '-') === totalTable[j].name) {
                 totalTable[j].poloniexPrice = parseFloat(poloniex.poloniexLastPriceArray[i].price);
                 break;
             }
         }
     }
 
-    for(let i = 0; i < bittrex.bittrexLastPriceArray.length; i++) {
-        for(let j = 0; j < totalTable.length; j++) {
-            if(bittrex.bittrexLastPriceArray[i].name === totalTable[j].name) {
+    for (let i = 0; i < bittrex.bittrexLastPriceArray.length; i++) {
+        for (let j = 0; j < totalTable.length; j++) {
+            if (bittrex.bittrexLastPriceArray[i].name === totalTable[j].name) {
                 totalTable[j].bittrexPrice = bittrex.bittrexLastPriceArray[i].price;
                 break;
             }
         }
     }
 
-    for(let i = 0; i < totalTable.length; i++) {    // Percent 계산하기
-        if(totalTable[i].poloniexPrice && totalTable[i].bittrexPrice) {
+    for (let i = 0; i < totalTable.length; i++) {    // Percent 계산하기
+        if (totalTable[i].poloniexPrice && totalTable[i].bittrexPrice) {
             totalTable[i].percent = ((totalTable[i].poloniexPrice - totalTable[i].bittrexPrice) / totalTable[i].poloniexPrice * 100).toFixed(2);
         }
     }
 
-    for(let i = 0; i < totalTable.length; i++) {    // 비어있는 것 삭제
-        if(!(totalTable[i].poloniexPrice && totalTable[i].bittrexPrice)) {
+    for (let i = 0; i < totalTable.length; i++) {    // 비어있는 것 삭제
+        if (!(totalTable[i].poloniexPrice && totalTable[i].bittrexPrice)) {
             totalTable.splice(i, 1);
             i--;
         }
